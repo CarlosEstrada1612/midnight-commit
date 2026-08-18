071a5fb826acd4119f7e037fe876e8fa26d3939d
Artulool <car.estrada.re@gmail.com>
2026-08-17 23:32:49 -0500

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Midnight Commit';
+export const SITE_DESCRIPTION = ' > Welcome to midnight commit // thoughts, experiments and random stuff about technology and life!';
